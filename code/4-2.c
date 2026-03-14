8b5adb0d185a1ce60673e023727e9b7afb49134e
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-14 10:41:35 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -3,8 +3,8 @@
 int main()
 {
     int i, j;
-    for (i = 1;i < 9;i++) {
-        for (j = 1;j < 9;j++) {
+    for (i = 1;i <= 9;i++) {
+        for (j = 1;j <= 9;j++) {
             printf("%d * %d = %d\n", i, j, i*j);
         }
     }
