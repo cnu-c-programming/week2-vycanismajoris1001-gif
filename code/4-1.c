c6189cdc83ac219774b65229389bd1a1e9e0ea30
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-14 10:45:33 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int n1, n2;
+    char c;
+
+    scanf("%d %d %c", &n1, &n2, &c);
+    
+    if (c == '+') {
+            printf("%d\n", n1 + n2);
+        } else if (c == '-') {
+            printf("%d\n", n1 - n2);
+        } else if (c == '*') {
+            printf("%d\n", n1 * n2);
+        } else if (c == '/') {
+            if (num2 != 0) {
+                printf("%d\n", n1 / n2);
+            } else {
+                printf("Error: Division by zero\n");
+            }
+        }
 
     return 0;
-}
-
+    }
