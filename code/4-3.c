1820e2f58d74569a917ecf32653d7e24cc80024f
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-14 10:52:38 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int n;
+    bool b = true;
 
+    if (scanf("%d", &n) != 1) 
+        return 0;
+
+    if (n < 2) {
+        b = false;
+    } else {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                b = false;
+                break;
+            }
+        }
+    }
+
+    if (b) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
     return 0;
 }
-
